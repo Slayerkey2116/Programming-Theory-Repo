182a0c02b238e434460ad060ba37b9bd599c8911
Slayerkey2116 <97804300+Slayerkey2116@users.noreply.github.com>
2022-01-22 21:09:18 -0500

    Created scripts

---
+++ Programming Theory Project/Assets/Scripts/BigEnemy.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigEnemy : Enemy
+{
+
+    public override void Health()
+    {
+        health = 10;
+        pointValue = 5;
+        if (health <= 0)
+        {
+            gameObject.SetActive(false);
+            gameManager.UpdateScore(pointValue);
+        }
+    }
+
+    public override void Attack()
+    {
+        hp.Health -= 3;
+    }
+
+    public override void Move()
+    {        
+        speed = 5;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Attack();
+        Health();
+        Move();
+    }
+}
---
+++ Programming Theory Project/Assets/Scripts/BulletPooler.cs
@@ -0,0 +1,44 @@
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class BulletPooler : MonoBehaviour
+    {
+        public static BulletPooler SharedInstance;
+        public List<GameObject> pooledObjects;
+        public GameObject objectToPool;
+        public int amountToPool;
+
+       public void Awake()
+        {
+            SharedInstance = this;
+        }
+
+       public void Start()
+        {
+            // Loop through list of pooled objects,deactivating them and adding them to the list 
+            pooledObjects = new List<GameObject>();
+            for (int i = 0; i < amountToPool; i++)
+            {
+                GameObject obj = (GameObject)Instantiate(objectToPool);
+                obj.SetActive(false);
+                pooledObjects.Add(obj);
+                obj.transform.SetParent(this.transform); // set as children of Spawn Manager
+            }
+        }
+
+        public GameObject GetPooledObject()
+        {
+            // For as many objects as are in the pooledObjects list
+            for (int i = 0; i < pooledObjects.Count; i++)
+            {
+                // if the pooled objects is NOT active, return that object 
+                if (!pooledObjects[i].activeInHierarchy)
+                {
+                    return pooledObjects[i];
+                }
+            }
+            // otherwise, return null   
+            return null;
+        }
+    }
